774bb637f2b3caab618c11ab4075acaa59505f9d
steveo9219 <stephen.guzman92@gmail.com>
2020-05-19 22:16:26 -0500

    Readme was update with links

--- Assets/script.js
+++ Assets/script.js
@@ -6,68 +6,73 @@ var startButton = document.getElementById("generate");
 var answerA = document.getElementById(".btn");
 var timer = document.getElementById("#timer");
 var startButton = document.getElementById("startButton");
+var scoreBoardEl = document.getElementById("highScores");
+
+function myTimer() {
+  var d = new Date();
+  document.getElementById("timer").innerHTML = d.toLocaleTimeString();
+}
 
 var questionIndex = 0;
 var i = 0;
 var secondsLeft = 75;
+var score = 0;
+var questionArray = 0;
+
 var questionsArray = [
   {
     question: "Inside which HTML element do we put the JavaScript?",
     choices: ["1. <script>", "2. <js>", "3. <javascript>"],
-    answer: 0,
+    answer: "a",
   },
   {
     question: "JavaScript is the same as Java.",
     choices: ["1. True", "2. False", "3. Maybe"],
-    answer: 0,
+    answer: "a",
   },
   {
     question: "Which event occurs when the user clicks on an HTML element?",
     choices: ["1. onchange", "2. onclick", "3. onmouseover"],
-    answer: 1,
+    answer: "b",
   },
   {
     question: "How do you declare a JavaScript variable?",
     choices: ["1. var CarName", "2. variable carName", "3. v carName"],
-    answer: 0,
+    answer: "a",
   },
 ];
-// this.startButton.addEventListener("click")
-// var FirstQuestionAnswer = questionsArray.choices[questionsArray.answer]; //nested array
-// console.log(questionsArray.answer);
-// let firstAnswerChoice = questionsArray[questionIndex].choices[0];
 
-//console.log([questionsArray][(0, 0)]);
+firstAnswer.addEventListener("click", increaseQuestionIndex);
+secondAnswer.addEventListener("click", increaseQuestionIndex);
+thirdAnswer.addEventListener("click", increaseQuestionIndex);
 
 startButton.addEventListener("click", function () {
   startButton.outerHTML = "";
   displayPage();
 });
 //this will increase the question after clicking an answer but i need to log the answer first
-// function increaseQuestionIndex() {
-//   //console.log(event.target.value);
-//   questionIndex++;
-//   displayPage();
-// }
+function increaseQuestionIndex() {
+  var choice = this.getAttribute("data-value");
+  console.log(choice);
+  console.log(questionsArray[0].answer);
+  //let q = questionArray[questionIndex];
+  if (choice === questionsArray[0].answer) {
+    score++;
+
+    scoreBoardEl.innerText = score;
+
+    console.log(score);
+  } else {
+    console.log("wrong");
+  }
+
+  questionIndex++;
+  displayPage();
+}
 
 function displayPage() {
   question.innerHTML = questionsArray[questionIndex].question;
   firstAnswer.textContent = questionsArray[questionIndex].choices[0];
   secondAnswer.textContent = questionsArray[questionIndex].choices[1];
   thirdAnswer.textContent = questionsArray[questionIndex].choices[2];
-  assignValue();
 }
-
-// function assignValue() {
-//   if (firstAnswerChoice === 0) {
-//     console.log("pizzatime"); //score = score+1
-//   } else if {
-//     console.log("notpizzatime");
-//   }
-// }
-
-// function assignValue() {}
-//firstAnswerChoice.setAttribute(firstAnswerChoice, {0 {i}.true})
-//know where element is and grab value. id is unique
-
-//.setAttribute(value, {THIS IS WHERE YOU PUT THE CURRENT QUESTION INDEX CHOICE{INDEX}.WRONG OR CORRECT})
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Timed-Code-Quiz
+# Timed-Code-Quiz READ ME
+
+i really had a tough time with this one obviously.
+i got help from many people but and i was able to apply
+what i wanted to do for the first question and kind of the point system
+but i couldnt figure out how to apply for the next set of questions.
+i will try to revisit this homework one day. but i spent close to the 20 hours on
+this HW and i think its time to move on and revisit at a later date with more knowledge!!!
+
+and suggestions to this would go a long way. but
+thanks
+
+github repo: https://github.com/steveo9219/Stephens-Timed-Code-Quiz
+URL for code quiz: https://steveo9219.github.io/Stephens-Timed-Code-Quiz/
+Code Quiz Screenshot https://drive.google.com/file/d/18ZU8i0OAry3aNk4DNUh1aJvTPiFcz8aZ/view?usp=sharing
